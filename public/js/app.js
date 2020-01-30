c1048f7f82baea3f5584228c657e1bdd0343d11a
Alex .M <amirhesam.kouhestani@gmail.com>
2020-01-31 00:07:48 +0330

    Remove unnecessary lines

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,3 @@
-console.log('Front end javascript!')
-
-// fetch('http://puzzle.mead.io/puzzle').then((response) => {
-//     response.json().then((data) => {
-//         console.log(data)
-//     })
-// })
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
@@ -19,14 +11,14 @@ weatherForm.addEventListener('submit', (e) => {
     messageTwo.textContent = ''
 
     fetch(`http://localhost:3000/weather?address=${adress}`).then((response) => {
-    response.json().then((data) => {
-        if (data.error) {
-            messageOne.textContent = data.error
-        } else {
-            messageOne.textContent = data.location
-            messageTwo.textContent = data.forecast
-        }
+        response.json().then((data) => {
+            if (data.error) {
+                messageOne.textContent = data.error
+            } else {
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
+            }
+        })
     })
-})
 
 })
