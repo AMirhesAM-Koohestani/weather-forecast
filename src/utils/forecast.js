9caff50c013eb955ad693ed1b11dd831df6a5f37
Alex .M <amirhesam.kouhestani@gmail.com>
2020-02-05 22:11:52 +0330

    Add high and low to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -4,16 +4,26 @@ const forecast = (latitude, longitude, callback) => {
 
     const url = `https://api.darksky.net/forecast/4f048e7e6fac102e7ab5fd01010ada74/${latitude},${longitude}?units=si`
 
-    request({ url, json: true }, (error, {body} = {}) => {
+    request({ url, json: true }, (error, { body } = {}) => {
         if (error) {
             callback('Unable to connect to weather service!')
         } else if (body.error) {
             callback('Poorely formatted request!')
         } else {
-            const {precipProbability: rainChance, temperature} = body.currently
-            callback(undefined, `${body.daily.data[0].summary} It is currently ${temperature} degrees out. There is a ${rainChance * 100} % chance of rain.`)
+            const { precipProbability: rainChance, temperature } = body.currently
+            const { temperatureHigh: highTemp, temperatureHighTime: highTime, temperatureLow: lowTemp, temperatureLowTime: lowTime, summary: todaySummary } = body.daily.data[0]
+            callback(undefined,
+                `${todaySummary}
+It is currently ${temperature}° out.
+There is a ${rainChance * 100} % chance of rain.
+Maximum temperature is ${highTemp}° around ${toRealHour(highTime)} o'clock with minimum of ${lowTemp}° around ${toRealHour(lowTime)} o'clock.`)
         }
     })
+}
+
+const toRealHour = (timestamp) => {
+    const date = new Date(timestamp * 1000);
+    return date.getHours();
 }
 
 module.exports = forecast
